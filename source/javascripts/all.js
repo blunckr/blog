6caf6af2a53392a8fd5a84d44d8327b423a68a56
Ryker Blunck <rykerblunck@gmail.com>
2016-11-18 14:04:00 -0700

    more legit js

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,31 +1,53 @@
-'use strict'
+'use strict';
 
-function ajax(url, done) {
-  var httpRequest = new XMLHttpRequest()
-  httpRequest.onreadystatechange = function(){
-    if (httpRequest.readyState === XMLHttpRequest.DONE) {
-      if (httpRequest.status === 200) {
-        done(httpRequest.responseText)
-      } else {
-        alert('There was a problem with the request.')
+(function(){
+  function ajax(url, done) {
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function(){
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          done(httpRequest.responseText);
+        } else {
+          alert('There was a problem with the request.');
+        }
       }
     }
+    httpRequest.open('GET', url);
+    httpRequest.send();
   }
-  httpRequest.open('GET', url);
-  httpRequest.send();
-}
 
-document.onclick = function(e){
-  var el = e.target
-  if(el.tagName === 'A'){
+  function loadBody(newPage){
+    var body = newPage.querySelector('body');
+    document.body.innerHTML = body.innerHTML;
+  }
+
+  function updateTitle(newPage){
+    document.title = newPage.querySelector('title').innerText;
+  }
+
+  function ghettoLink(e){
+    var el = e.target;
     if(window.location.host === el.host){
       ajax(el.href, function(responseText){
-        var wrapper = document.createElement('html')
-        wrapper.innerHTML = responseText
-        var body = wrapper.getElementsByTagName('body')[0]
-        document.body.innerHTML = body.innerHTML
-      })
-      return false
+        var newPage = document.createElement('html');
+        newPage.innerHTML = responseText;
+        loadBody(newPage);
+        updateTitle(newPage);
+        history.pushState({}, '', el.href);
+        pageLoad();
+      });
+      e.preventDefault();
     }
   }
-}
+
+  function pageLoad(){
+    var anchors = document.getElementsByTagName('a');
+    for(var i = 0; i < anchors.length; i++) {
+      anchors[i].addEventListener('click', ghettoLink);
+    }
+  }
+
+  window.Blunckr = {
+    pageLoad: pageLoad
+  };
+})();
