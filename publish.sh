d424e6f7631b04cacf70471fb6ac8e9ec3ee6f27
Ryker Blunck <rykerblunck@gmail.com>
2016-11-19 12:11:37 -0700

    don't need to add origin anymore

--- publish.sh
+++ publish.sh
@@ -5,6 +5,5 @@ rm -rf *
 cp -r ../blog/build/* ./
 git add .
 git commit --allow-empty-message -m ''
-git remote add origin git@github.com:blunckr/blunckr.github.io.git
 git push -u --force origin master
 
